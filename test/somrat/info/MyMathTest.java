cd3a91002e7d1df4ad8eba43d368c70855ecd4c2
somratcste <somratcste@gmail.com>
2018-12-19 18:54:16 +0600

    add more annotation

--- test/somrat/info/MyMathTest.java
+++ test/somrat/info/MyMathTest.java
@@ -2,10 +2,34 @@ package somrat.info;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
 	MyMath myMath = new MyMath();
+	
+	@Before
+	public void before() {
+		System.out.println("before");
+	}
+	
+	@After
+	public void after() {
+		System.out.println("after");
+	}
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("before class");
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		System.out.println("after class");
+	}
 
 	@Test
 	public void sum_with3numbers() {
